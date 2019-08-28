b284d18277731445a071a16654f9741843a4c45f
abrown2644 <abrown2644@gmail.com>
2019-08-27 20:12:17 -0400

    styling changes before first build

--- src/components/bio.js
+++ src/components/bio.js
@@ -13,7 +13,7 @@ class Bio extends React.Component {
              <div className={styles.bioButtons}>
                  <ul>
                      <li id={styles.linkOne} ><a href="mailto:abrown2644@gmail.com"><FontAwesomeIcon icon={faEnvelope} size="3x" className={styles.bioIcon} title={"Email Me"} alt="Email Me"/></a><p className={styles.bioButtonsText}>Email Me</p></li>
-                     <li id={styles.linkTwo} ><a href="https://www.linkedin.com/in/andrew-brown2644/" rel="noopen" target="_blank"><FontAwesomeIcon icon={faLinkedinIn} size="3x" className={styles.bioIcon} title={"See My LinkedIn"} alt="My LinkedIn"/></a><p className={styles.bioButtonsText}>LinkedIn</p></li>
+                     <li id={styles.linkTwo} ><a href="https://www.linkedin.com/in/andrew-brown2644/" rel="noopener" target="_blank"><FontAwesomeIcon icon={faLinkedinIn} size="3x" className={styles.bioIcon} title={"See My LinkedIn"} alt="My LinkedIn"/></a><p className={styles.bioButtonsText}>LinkedIn</p></li>
                      <li id={styles.linkThree} ><a href="../images/resume.pfd"><FontAwesomeIcon icon={faArrowDown} size="3x" className={styles.bioIcon} title={"Download Resume"} alt="Download Resume"/></a><p className={styles.bioButtonsText}>Resume</p></li>
                  </ul>
              </div>

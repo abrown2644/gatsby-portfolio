14709129b536a7fa447fcd7d4b8165fad4d77a63
abrown2644 <abrown2644@gmail.com>
2019-08-29 10:20:16 -0400

    make gogo

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
         icon: `src/images/icon-big.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // this (optional) plugin enables Progressive Web App + Offline functionality NOICE!
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
   ],
